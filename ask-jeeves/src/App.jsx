effe15062aa1e36beea645469ff91dbacaa76105
Yohanes Demissie <yohanesdemissie@Yohaness-MacBook-Pro.local>
2025-12-13 23:11:59 -0800

    completed interfaace layout, theme, and responsiveness. Next step: gemini API

--- ask-jeeves/src/App.jsx
+++ ask-jeeves/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Sidebar from './components/sidebar/sidebar';
+import Sidebar from './components/Sidebar/sidebar';
+import Main from './components/Main/main';
 
 const App = () => {
   return (
     <>
       <Sidebar />
+      <Main />
     </>
   );
 };
